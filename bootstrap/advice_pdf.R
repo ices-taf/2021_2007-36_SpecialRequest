e508e3f48689c16563e69eddbf0bbe49a7eb4cbb
Colin Millar <colin.millar@ices.dk>
2021-09-24 13:36:32 +0200

    correct advice pdf url

--- bootstrap/advice_pdf.R
+++ bootstrap/advice_pdf.R
@@ -8,6 +8,6 @@
 #' @tafOriginator ICES
 #' @tafYear 2021
 #' @tafAccess Public
-#' @tafSource https://www.ices.dk/sites/pub/Publication%20Reports/Advice/2021/Special_Requests/ospar.2021.12.pdf
+#' @tafSource https://www.ices.dk/sites/pub/Publication%20Reports/Advice/2021/Special_Requests/ospar.2021.11.pdf
 
 NULL
